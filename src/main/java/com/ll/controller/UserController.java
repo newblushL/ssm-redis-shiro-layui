fc3d60b1532e01012100b9dbbb61d6e1f3bc23c1
LL <844457434@qq.com>
2019-05-14 17:18:47 +0800

    加入shiro和layui

--- src/main/java/com/ll/controller/UserController.java
+++ src/main/java/com/ll/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ll.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.ll.entity.UserInfo;
 import com.ll.service.UserService;
 import org.slf4j.Logger;
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +25,8 @@ public class UserController {
     private static Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
+    @Autowired
+    private Producer captchaProducer = null;
 
     @RequestMapping(value = "/findUserById", method = RequestMethod.GET)
     @ResponseBody
@@ -31,4 +38,22 @@ public class UserController {
         //System.out.println(userInfo.getUsername()+userInfo.getPassword()+userInfo.getAge()+userInfo.getId());
         return map;
     }
+
+    @RequestMapping(value="/vcode",method = RequestMethod.GET)
+    public void vcode(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+//		VerifyCode vc = new VerifyCode();
+//		BufferedImage image = vc.getImage();// 获取一次性验证码图片
+        String text = captchaProducer.createText();
+        BufferedImage image = captchaProducer.createImage(text);
+        // 该方法必须在getImage()方法之后来调用
+        // System.out.println("验证码图片上的文本:"+vc.getText());//获取图片上的文本
+        // 把文本保存到session中，为验证做准备
+        //req.getSession().setAttribute("vcode", vc.getText());
+        //保存到shiro session
+//        ShiroUtils.setSessionAttribute("kaptcha", text);
+        req.getSession().setAttribute("kaptcha",text);
+        //VerifyCode.output(image, resp.getOutputStream());// 把图片写到指定流中
+        ImageIO.write(image, "JPEG", resp.getOutputStream());
+    }
+
 }
---
+++ src/main/java/com/ll/shiro/CustomRealm.java
@@ -0,0 +1,19 @@
+package com.ll.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class CustomRealm extends AuthorizingRealm {
+
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
